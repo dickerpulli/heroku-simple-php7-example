7045a484e1be8c11f4736d9b376b9b2b6debee10
Thomas Bosch <tbosch@gmx.de>
2018-11-16 12:58:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,27 +3,39 @@
   
   function loadTodos() {
     $.ajax({
-      url: '',
-      success: function() {
+      url: '/todos.php',
+      success: function(resp) {
+        $('todo-items').html(resp);
       },
-      error: function() {
+      error: function(resp) {
+        showErrors(resp.message);
       }
     });
   }
-
-  $(document).ready(function() {
+  
+  function showErrors(messages) {
     $errorsAlert = $('#errors').parent('div.alert');
     $errorsSpan = $('#errors');
+    $errorsSpan.text(messages);
+    $errorsAlert.show();
+  }
+  
+  function hideErrors() {
+    $errorsAlert = $('#errors').parent('div.alert');
+    $errorsSpan = $('#errors');
+    $errorsAlert.hide();
+    $errorsSpan.text('');
+  }
+
+  $(document).ready(function() {
     $('form#add-todo').submit(function(event) {
       $(this).preventDefault();
       const text = $('#text').val();
       const count = $('#count').val();
-      $errorsSpan.text(text + ' : ' + count);
-      $errorsAlert.show();
+      showErrors(text + ' : ' + count);
     });
     $errorsAlert.find('button.close').click(function() {
-      $errorsAlert.hide();
-      $errorsSpan.text('');
+      hideErrors();
     });
   });
 })();
