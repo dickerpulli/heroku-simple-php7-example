d11c089b0a5851b61c70b39399cb9df7abf501f4
Thomas Bosch <tbosch@gmx.de>
2018-11-16 12:45:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,18 @@
   'use strict';
 
   $(document).ready(function() {
+    $errorsAlert = $('#errors').parent('div.alert');
+    $errorsSpan = $('#errors');
     $('form#add-todo').submit(function(event) {
       event.preventDefault();
       const text = $('#text').val();
       const count = $('#count').val();
-      $('#errors').text(text + ' : ' + count);
-      $('#errors').parent('div.alert').show();
+      $errorsSpan.text(text + ' : ' + count);
+      $errorsAlert.show();
+    });
+    $errorsAlert.find('button.close').click(function() {
+      $errorsAlert.hide();
+      $errorsSpan.text('');
     });
   });
 })();
